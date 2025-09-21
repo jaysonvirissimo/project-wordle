7fd7f0d12ee81c5490715df2fe35e929ff6d05bf
Jayson Virissimo <jayson.virissimo@asu.edu>
2025-09-20 18:29:51 -0700

    Implement GuessInput component

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function GuessInput() {
+  const requiredLength = 5;
+  const [guess, setGuess] = React.useState("");
+ 
+
+  function onSubmit(event) {
+    event.preventDefault();
+    console.log(guess.toUpperCase());
+    setGuess("");
+  }
+
+  return (<form className="guess-input-wrapper" onSubmit={onSubmit}>
+    <label htmlFor="guess-input">Enter guess:</label>
+    <input id="guess-input" maxLength={requiredLength} minLength={requiredLength} onChange={(event) => setGuess(event.target.value)} type="text" value={guess} />
+  </form>);
+}
+
+export default GuessInput;
